dc63638d697b3f5f2a0b7d69420f84fada4a6524
escm1990 <escm.1990@gmail.com>
2020-12-03 20:19:58 -0600

    2020-12-03

---
+++ Tareas/T5_Ejercicio1.py
@@ -0,0 +1,14 @@
+#Ejercicios de concatenación
+
+print("-----EJERCICIO 1-------")
+cadena = "Bienvenidos al curso de Python"
+cambio = "b" + cadena[1:]
+print(cambio) 
+
+#############################
+
+print("\n\n-----EJERCICIO 2-------")    
+cadena2 = "esto es un texto"
+numero = 777
+cambio2 = "Hola. E" + cadena2[1:] + " y le contateno el número "+str(numero)
+print(cambio2)
